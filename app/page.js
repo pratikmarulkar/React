ab17bb5b7d54640c3ff3905ce575ef49b5d9996e
pratikmarulkar1598 <pratikvbaisolutions@gmail.com>
2024-05-09 15:14:55 +0530

    Icons

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 'use client'
 import React, { useState, useEffect } from 'react';
-import { Card, Text, Group, Divider, Button } from '@mantine/core'; // Import necessary components
+import { Card, Text, Group, Divider, Button } from '@mantine/core'; 
 import { Star, StarOff } from '@tabler/icons-react';
 import { IconMail, IconPhone, IconWorld } from '@tabler/icons-react';
+import { IconHttpDelete } from '@tabler/icons-react';
 
 function UserList() {
   const [users, setUsers] = useState([]);
@@ -84,8 +85,8 @@ function UserList() {
                 <a href={user.website}>{user.website}</a>
               </Group>
               <Group style={{ justifyContent: 'space-between', marginTop: 'auto' }}>
-                <Button onClick={() => handleFollow(user.id)}>{user.isFollowing ? 'Unfollow' : 'Follow'}</Button>
-                <Button onClick={() => handleDelete(user.id)} >Delete</Button>
+                <Button onClick={() => handleFollow(user.id)}>  <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-user-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M16 19h6" /><path d="M19 16v6" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4" /></svg>{user.isFollowing ? 'Unfollow' : 'Follow'}</Button>
+                <Button onClick={() => handleDelete(user.id)}  > <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>Delete</Button>
               </Group>
             </Group>
           </Card>
